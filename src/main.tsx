12aa8bf7842038e6ecacdbedf6c73da776afd920
Lucian Stoian <lucians@ENSRO0414.local>
2024-12-26 12:36:57 +0200

    feature/KAN-5-implement-toast-component

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,20 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext/AuthContext.tsx";
+import { ToastContextProvider } from "./context/toastContext/ToastContext.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
-      </QueryClientProvider>
+      <ToastContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthContextProvider>
+            <App />
+          </AuthContextProvider>
+        </QueryClientProvider>
+      </ToastContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
