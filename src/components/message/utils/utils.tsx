9bdd43c0812ef161771b43cf4ce241077a3bdf07
lucians-ens <lucians@ensemble.com>
2025-02-28 17:09:50 +0200

    Update src/components/message/utils/utils.tsx
    Co-authored-by: valeriat-ens <141741069+valeriat-ens@users.noreply.github.com>

--- src/components/message/utils/utils.tsx
+++ src/components/message/utils/utils.tsx
@@ -1,4 +1,4 @@
 export const Messages = {
     SUCCESS_ACCOUNT_ACTIVATION: "Your account has been activated",
-    ERROR_ACCOUNT_ACTIVATION: "There was a problem activating you account"
+    ERROR_ACCOUNT_ACTIVATION: "There was a problem activating your account"
 }
